3c79288b618ddcf8fa3de1613498d475312f556c
Lennard Häring <haering.lennard@gmail.com>
2022-07-29 18:03:48 +0200

    added classes VerticalFarm, PlantTray, SinglePlantUnit

--- Unity/Assets/Scripts/VFBuilder.cs
+++ Unity/Assets/Scripts/VFBuilder.cs
@@ -1,12 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class VFBuilder : MonoBehaviour
 {
     
     [SerializeField] private GameObject cube;
+
+    [SerializeField] public int traySizeX;
+    [SerializeField] public int traySizeY;
+    
+    public class VerticalFarm
+    {
+        
+    }
+
+    public class PlantTray
+    {
+        private int _sizeX;
+        private int _sizeY;
+
+        public PlantTray(int sizeX,int sizeY)
+        {
+            _sizeX = sizeX;
+            _sizeY = sizeX;
+        }
+    }
+
+    public class SinglePlantUnit
+    {
+        
+    }
     
     void Start()
     {
