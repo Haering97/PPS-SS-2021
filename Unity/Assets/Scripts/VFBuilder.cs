e2214166be27593d17bb42367b9ba17f68d18075
Lennard Häring <haering.lennard@gmail.com>
2022-08-01 15:30:15 +0200

    add classes

--- Unity/Assets/Scripts/VFBuilder.cs
+++ Unity/Assets/Scripts/VFBuilder.cs
@@ -12,35 +12,64 @@ public class VFBuilder : MonoBehaviour
     [SerializeField] public int traySizeX;
     [SerializeField] public int traySizeY;
     
+    
+    
     public class VerticalFarm
     {
-        
+        private Shelf[] Shelves { get; set; }
     }
 
-    public class PlantTray
+    public class Shelf
+    {
+        private int _height;
+        private int _length;
+    }
+    
+    public class Tray
     {
         private int _sizeX;
         private int _sizeY;
-
-        public PlantTray(int sizeX,int sizeY)
+        private SinglePlantUnit[][] _plantUnits;
+        
+        public Tray(int sizeX,int sizeY)
         {
             _sizeX = sizeX;
-            _sizeY = sizeX;
+            _sizeY = sizeY;
         }
     }
 
     public class SinglePlantUnit
     {
-        
+        public SinglePlantUnit(float humidity, float growthFactor)
+        {
+            Humidity = humidity;
+            GrowthFactor = growthFactor;
+        }
+
+        public float Humidity { get; set; }
+        public float GrowthFactor { get; set; } = 0;
     }
+    
+    
     
     void Start()
     {
         Debug.Log("PP-Log: VFBuilder instantiate cube");
         Instantiate(cube,this.transform);
+        
     }
     
     void Update()
+    {
+        
+    }
+
+    void constructVF()
+    {
+        
+    }
+
+    void instantiateVF()
     {
         
     }
