ebbf4374ddae28a312725da8316a6d0a517dfeb3
Lennard Häing <haering.lennard@gmail.com>
2022-09-29 15:49:05 +0200

    add scripts

---
+++ Unity/Assets/Scripts/PreFab Scripts/ShelfScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelfScript : MonoBehaviour
+{
+    public int id;
+    public int shelfHeight;
+    public int shelfLength;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
