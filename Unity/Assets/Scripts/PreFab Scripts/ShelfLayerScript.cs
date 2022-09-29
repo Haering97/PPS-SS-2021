8c34ebdf4eb9362def745985f3a99636227f9132
Lennard Häing <haering.lennard@gmail.com>
2022-09-29 16:18:20 +0200

    adding ShelfLayer and Tray Script

---
+++ Unity/Assets/Scripts/PreFab Scripts/ShelfLayerScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelfLayerScript : MonoBehaviour
+{   
+    
+    [SerializeField] private GameObject tray;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
