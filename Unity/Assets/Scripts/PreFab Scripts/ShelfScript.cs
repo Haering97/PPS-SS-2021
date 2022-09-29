8f9a936022b3e60a92a0d61e7818e235fe204f72
Lennard Häing <haering.lennard@gmail.com>
2022-09-29 16:33:06 +0200

    instantiate shelfs

--- Unity/Assets/Scripts/PreFab Scripts/ShelfScript.cs
+++ Unity/Assets/Scripts/PreFab Scripts/ShelfScript.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class ShelfScript : MonoBehaviour
 {
+    
+    [SerializeField] private GameObject ShelfLayer;
     public int id;
     public int shelfHeight;
     public int shelfLength;
     void Start()
     {
-        
+        for (int i = 0; i < shelfHeight; i++)
+        {
+            var shelfLayerInstance= Instantiate(ShelfLayer, transform);
+            var shelfLayerScript = shelfLayerInstance.GetComponent<ShelfLayerScript>();
+            shelfLayerScript.shelfLength = shelfLength;
+        }
     }
 
     // Update is called once per frame
