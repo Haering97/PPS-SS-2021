c543e08fd734dac21f1ac93a2f4d375d1247730f
Lennard Häing <haering.lennard@gmail.com>
2022-09-29 16:34:00 +0200

    adding shelflayer and tray

--- Unity/Assets/Scripts/PreFab Scripts/ShelfLayerScript.cs
+++ Unity/Assets/Scripts/PreFab Scripts/ShelfLayerScript.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShelfLayerScript : MonoBehaviour
-{   
-    
+{
     [SerializeField] private GameObject tray;
+
+    public int id;
+    public int shelfLength;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < shelfLength; i++)
+        {
+            var trayInstance = Instantiate(tray, transform);
+            var trayScript = trayInstance.GetComponent<TrayScript>();
+            //TODO stat durchscleifen daten vom Manager beziehen.
+        }
     }
 
     // Update is called once per frame
--- Unity/Assets/Scripts/PreFab Scripts/TrayScript.cs
+++ Unity/Assets/Scripts/PreFab Scripts/TrayScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TrayScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject cube;
     void Start()
     {
         
