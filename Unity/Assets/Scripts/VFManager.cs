95af3612ebabf8a8a6a752c029bc1caf2a4ccaa2
Haering97 <haering.lennard@gmail.com>
2022-10-04 17:37:20 +0200

    refactor offsets

--- Unity/Assets/Scripts/VFManager.cs
+++ Unity/Assets/Scripts/VFManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class VFManager : MonoBehaviour
@@ -23,25 +24,35 @@ public class VFManager : MonoBehaviour
     
     public float globalsize = 1f;
 
-    
-    
+    public GameObject[] shelves;
+     
     void Start()
     {
         vfOrigin = transform;
         _spacingShelvesDynamic = spacingShelves + trayWidth;
         //spacingTraysDynamic = spacingTrays + trayLength;
-        
+        shelves = new GameObject[numberOfShelves];
          
-
-        
         
         for (int i = 0; i < numberOfShelves; i++)
         {
             var shelfInstance = Instantiate(shelf, vfOrigin);
             shelfInstance.transform.position += new Vector3(i, 0, 0) * _spacingShelvesDynamic;
+            /*
+            Debug.Log("RootOffset");
+            Debug.Log("X:  "+getRootOffset().x);
+            Debug.Log("Z:  "+getRootOffset().z);
+            */
+            shelfInstance.transform.position -= getRootOffset() / 2;
             var shelfScript = shelfInstance.GetComponent<ShelfScript>();
             shelfScript.id = i;
+
+            shelves[i] = shelfInstance;
         }
+        
+        
+        Debug.Log("Farm Instantiated");
+        //Testing
         
 
     }
@@ -58,11 +69,30 @@ public class VFManager : MonoBehaviour
 
     public Vector3 getTraySize()
     {
-        return new Vector3(trayWidth + (trayWidth * spacingPlants), 1f, trayLength + (trayWidth * spacingPlants));
+        return new Vector3(
+            trayWidth + (trayWidth * spacingPlants), 
+            1f,
+            trayLength + (trayWidth * spacingPlants));
     }
 
     public Vector3 getOffset()
     {
-        return (new Vector3((trayWidth-1) +(trayWidth * spacingPlants), 0, (trayLength-1) + (trayLength * spacingPlants)) * globalsize);
+        return (new Vector3(
+            (trayWidth-1) +(trayWidth * spacingPlants), 
+            0, 
+            (trayLength-1) + (trayLength * spacingPlants))
+                * globalsize);
     }
+    public Vector3 getRootOffset()
+    {
+        return (new Vector3(
+            (trayWidth * (numberOfShelves-1)) + (spacingShelves * (numberOfShelves-1)), 
+            0,
+            (trayLength * (shelfLength-1)) + (spacingTrays * (shelfLength-1)))
+                * globalsize);
+    }
+    
+    
 }
+
+//TODO namen des GameObject zu typ und id ändern damit man alles auch über .find finden kann.
