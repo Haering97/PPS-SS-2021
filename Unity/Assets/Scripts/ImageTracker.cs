6a5646ffdfcd71453650d2edd2f75a4cf31b3c3b
Haering97 <haering.lennard@gmail.com>
2022-10-04 19:23:46 +0200

    testing ar object placement

--- Unity/Assets/Scripts/ImageTracker.cs
+++ Unity/Assets/Scripts/ImageTracker.cs
@@ -8,14 +8,15 @@ using UnityEngine.XR.ARSubsystems;
 
 public class ImageTracker : MonoBehaviour
 {
-    [SerializeField]
-    [Tooltip("Image manager on the AR Session Origin")]
+    [SerializeField] [Tooltip("Image manager on the AR Session Origin")]
     ARTrackedImageManager m_TrackedImageManager;
-    [SerializeField]
-    [Tooltip("Reference Image Library")]
+
+    [SerializeField] [Tooltip("Reference Image Library")]
     XRReferenceImageLibrary m_ImageLibrary;
 
-    [SerializeField] private GameObject model; 
+    [SerializeField] private GameObject model;
+
+    private GameObject instance;
 
     void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
 
@@ -27,15 +28,18 @@ public class ImageTracker : MonoBehaviour
         {
             var imageName = trackedImage.referenceImage.name;
             Debug.Log("PP-Log: Image Tracked");
-            Debug.Log("PP-Log:"+trackedImage.referenceImage.name);
-            
-            var offsetPos = new Vector3(0f,0.1f,0);
+            Debug.Log("PP-Log:" + trackedImage.referenceImage.name);
+
+            var offsetPos = new Vector3(0f, 0.1f, 0);
             //var scaleFactor = 0.5f;
             //TODO Rotation auslagern und im UI bereitstellen
             //TODO Multiple Prefabs and referenceImages
             //TODO Dynamic Prefabs
 
-            var rootInstance = Instantiate(model, transform);
+            instance = Instantiate(model, trackedImage.transform.position, trackedImage.transform.rotation);
+            
+
+            
 
             /* OLD WAY 
             var newPrefab = Instantiate(model,
@@ -54,13 +58,13 @@ public class ImageTracker : MonoBehaviour
                 Debug.Log("PP-Log:" +m_ImageLibrary[i].name);
             }
             */
-
-
         }
 
         foreach (var updatedImage in eventArgs.updated)
         {
             //Debug.Log(updatedImage.trackableId);
+            instance.transform.position = updatedImage.transform.position;
+            instance.transform.rotation = updatedImage.transform.rotation;
             // Handle updated event
         }
 
