69063ef5273ca8eb1a42e4861540d7153359b8f5
Lennard Häing <haering.lennard@gmail.com>
2022-10-20 12:41:44 +0200

    add script for checking if Ui or the model farm was tapped

---
+++ Unity/Assets/Scripts/CheckUi.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class CheckUi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public System.Collections.Generic.List<UnityEngine.EventSystems.RaycastResult> castGR(Vector2 pos)
+    {
+        //Code to be place in a MonoBehaviour with a GraphicRaycaster component
+        GraphicRaycaster gr = this.GetComponent<GraphicRaycaster>();
+        //Create the PointerEventData with null for the EventSystem
+        PointerEventData ped = new PointerEventData(null);
+        //Set required parameters, in this case, mouse position
+        ped.position = pos;
+        //Create list to receive all results
+        List<RaycastResult> results = new List<RaycastResult>();
+        //Raycast it
+        gr.Raycast(ped, results);
+            
+        return results;
+    }
+    
+}
