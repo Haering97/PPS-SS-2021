6ff13beabc41868e34b94e3050cf8b0620db3529
Lennard Häing <haering.lennard@gmail.com>
2022-10-25 18:52:49 +0200

    implemented cube logic

--- Unity/Assets/Scripts/CheckUi.cs
+++ Unity/Assets/Scripts/CheckUi.cs
@@ -8,16 +8,26 @@ using UnityEngine.UI;
 
 public class CheckUi : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject buttonSG;
+    private VFManager vfManager;
     void Start()
     {
-        
+        buttonSG = GameObject.Find("ButtonSG");
+        vfManager = GameObject.Find("Root").GetComponent<VFManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Hier Blende ich den Button nur ein wenn einzelne Cubes zu sehen sind.
+        if (!vfManager.renderSingleCubes) 
+        {
+            buttonSG.SetActive(false);
+        }
+        else
+        {
+            buttonSG.SetActive(true);
+        }
     }
 
     public System.Collections.Generic.List<UnityEngine.EventSystems.RaycastResult> castGR(Vector2 pos)
--- Unity/Assets/Scripts/PreFab Scripts/CubeScript.cs
+++ Unity/Assets/Scripts/PreFab Scripts/CubeScript.cs
@@ -8,18 +8,33 @@ public class CubeScript : MonoBehaviour
     public float temp;
     public float growth;
 
+    private Vector3 calculatedGrowth;
+    
+    private GameObject cube;
+    
     private VFManager vfManager;
     
     void Start()
     {
+        cube = this.gameObject;
         vfManager = GameObject.Find("Root").GetComponent<VFManager>();
-
-        growth = Random.Range(0.8f, 1f);
+        growth = Random.Range(0.5f, 1.1f);
+        calculatedGrowth = cube.transform.localScale * growth;
     }
 
     
     void Update()
     {
+        
+        
+        if (vfManager.showGrowth)
+        {
+            cube.transform.localScale = calculatedGrowth;
+        }
+        else
+        {
+            cube.transform.localScale = new Vector3(vfManager.globalsize,vfManager.globalsize,vfManager.globalsize);
+        }
         
     }
 }
